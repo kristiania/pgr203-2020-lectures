50faf69836a5e2df5878ad786d0d89e335adcecb
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-09-23 11:50:00 +0200

    server should return content-lenght (failing) #4

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,5 +1,6 @@
 package no.kristiania.http;
 
+import javax.management.Query;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,33 +8,39 @@ import java.net.Socket;
 public class HttpServer {
 
     public HttpServer(int port) throws IOException {
+        // Opens a entry point to our program for network clients
         ServerSocket serverSocket = new ServerSocket(port);
 
-        new Thread(() -> {
+        // new Threads executes the code in a separate "thread", that is: In parallel
+        new Thread(() -> { // anonymous function with code that will be executed in parallel
             while (true) {
                 try {
-                    handleRequest(serverSocket.accept());
+                    // accept waits for a client to try to connect - blocks
+                    Socket clientSocket = serverSocket.accept();
+                    handleRequest(clientSocket);
                 } catch (IOException e) {
+                    // If something went wrong - print out exception and try again
                     e.printStackTrace();
                 }
             }
-        }).start();
+        }).start(); // Start the threads, so the code inside executes without block the current thread
     }
 
+    // This code will be executed for each client
     private void handleRequest(Socket clientSocket) throws IOException {
         String requestLine = HttpClient.readLine(clientSocket);
         System.out.println(requestLine);
+        // Example "GET /echo?body=hello HTTP/1.1"
 
         String requestTarget = requestLine.split(" ")[1];
+        // Example "/echo?body=hello"
         String statusCode = "200";
 
         int questionPos = requestTarget.indexOf('?');
         if (questionPos != -1) {
-            String queryString = requestTarget.substring(questionPos+1);
-            int equalPos = queryString.indexOf('=');
-            String parameterName = queryString.substring(0, equalPos);
-            String parameterValue = queryString.substring(equalPos+1);
-            statusCode = parameterValue;
+            // body=hello
+            QueryString queryString = new QueryString(requestTarget.substring(questionPos+1));
+            statusCode = queryString.getParameter("status");
         }
 
         String response = "HTTP/1.1 " + statusCode + " OK\r\n" +
@@ -42,6 +49,7 @@ public class HttpServer {
                 "\r\n" +
                 "Hello <strong>World</strong>!";
 
+        // Write the response back to the client
         clientSocket.getOutputStream().write(response.getBytes());
     }
 
