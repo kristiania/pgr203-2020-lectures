f4d07e473016a0bc9cec537d3480f2d81b87aef5
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-10-05 22:48:25 +0200

    ProductDao should list inserted products #11 (test passes)
    * This is a "shallow" implementation

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -6,10 +6,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ProductDao {
+
+    private ArrayList<String> products = new ArrayList<>();
 
     public static void main(String[] args) throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
@@ -41,10 +44,10 @@ public class ProductDao {
     }
 
     public void insert(String product) {
-
+        products.add(product);
     }
 
     public List<String> list() {
-        return null;
+        return products;
     }
 }
