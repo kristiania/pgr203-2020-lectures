b4d8bb8074d94819c469e445b642089f58bede49
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-10-05 22:57:24 +0200

    ProductDao should list inserted products #11
    * Saves in database but reads from memory

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -2,6 +2,7 @@ package no.kristiania.database;
 
 import org.postgresql.ds.PGSimpleDataSource;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -13,6 +14,11 @@ import java.util.Scanner;
 public class ProductDao {
 
     private ArrayList<String> products = new ArrayList<>();
+    private DataSource dataSource;
+
+    public ProductDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args) throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
@@ -43,7 +49,13 @@ public class ProductDao {
         }
     }
 
-    public void insert(String product) {
+    public void insert(String product) throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("INSERT INTO products (product_name) VALUES (?)")) {
+                statement.setString(1, product);
+                statement.executeUpdate();
+            }
+        }
         products.add(product);
     }
 
--- src/test/java/no/kristiania/database/ProductDaoTest.java
+++ src/test/java/no/kristiania/database/ProductDaoTest.java
@@ -1,7 +1,10 @@
 package no.kristiania.database;
 
+import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,8 +12,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ProductDaoTest {
 
     @Test
-    void shouldListInsertedProducts() {
-        ProductDao productDao = new ProductDao();
+    void shouldListInsertedProducts() throws SQLException {
+        JdbcDataSource dataSource = new JdbcDataSource();
+        dataSource.setUrl("jdbc:h2:mem:testdatabase;DB_CLOSE_DELAY=-1");
+        try (Connection connection = dataSource.getConnection()) {
+            connection.prepareStatement("create table products (product_name varchar)").executeUpdate();
+        }
+
+        ProductDao productDao = new ProductDao(dataSource);
         String product = exampleProduct();
         productDao.insert(product);
         assertThat(productDao.list()).contains(product);
