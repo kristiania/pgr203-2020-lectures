dbdd4cdb675b2a7af35f3b42b4a0011545f55ab0
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2020-10-07 12:35:53 +0200

    #12 Product DAO should list inserted products (fake implementation)

--- src/main/java/no/kristiania/database/ProductDao.java
+++ src/main/java/no/kristiania/database/ProductDao.java
@@ -7,10 +7,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class ProductDao {
+
+    private ArrayList<String> products = new ArrayList<>();
+
+    public void insert(String product) {
+        products.add(product);
+    }
+
+    public List<String> list() {
+        return products;
+    }
 
     public static void main(String[] args) throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
@@ -39,13 +50,5 @@ public class ProductDao {
                 }
             }
         }
-    }
-
-    public void insert(String product) {
-
-    }
-
-    public List<String> list() {
-        return null;
     }
 }
